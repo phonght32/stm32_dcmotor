f6fc9c81777f62e2d995a1676c8f7368bcaa04c2
thanhphong98 <thanhphongho1998@gmail.com>
2020-11-18 16:21:46 +0700

    add dcmotor init

---
+++ dcmotor.c
@@ -0,0 +1,90 @@
+#include "freertos/FreeRTOS.h"
+#include "freertos/queue.h"
+#include "freertos/semphr.h"
+#include "freertos/task.h"
+
+#include "stm_log.h"
+#include "include/dcmotor.h"
+
+#define DCMOTOR_INIT_ERR_STR			"dcmotor init error"
+#define DCMOTOR_START_ERR_STR			"dcmotor start error"
+#define DCMOTOR_STOP_ERR_STR			"dcmotor stop error"
+#define DCMOTOR_SET_PWM_FREQ_ERR_STR	"dcmotor set pwm frequency error"
+#define DCMOTOR_SET_PWM_DUTY_ERR_STR	"dcmotor set pwm duty cycle error"
+
+#define mutex_lock(x)               while (xSemaphoreTake(x, portMAX_DELAY) != pdPASS)
+#define mutex_unlock(x)             xSemaphoreGive(x)
+#define mutex_create()              xSemaphoreCreateMutex()
+#define mutex_destroy(x)            vQueueDelete(x)
+
+static const char* TAG = "DCMOTOR";
+#define DCMOTOR_CHECK(a, str, action)  if(!(a)) {                                           \
+        STM_LOGE(TAG,"%s:%d (%s):%s", __FILE__, __LINE__, __FUNCTION__, str);       		\
+        action;                                                                             \
+        }
+
+typedef struct dcmotor {
+	dcmotor_hardware_info_t 	hw_info;
+	bool 						dir;
+	uint32_t 					freq;
+	uint8_t 					duty;
+	SemaphoreHandle_t			lock;
+} dcmotor_t;
+
+void _dcmotor_cleanup(dcmotor_handle_t handle)
+{
+	free(handle);
+}
+
+dcmotor_handle_t dcmotor_init(dcmotor_cfg_t *config)
+{
+	/* Check input conditions */
+	DCMOTOR_CHECK(config, DCMOTOR_INIT_ERR_STR, return NULL);
+	DCMOTOR_CHECK(config->hw_info.a_timer_num < TIMER_NUM_MAX, DCMOTOR_INIT_ERR_STR, return NULL);
+	DCMOTOR_CHECK(config->hw_info.a_timer_chnl < TIMER_CHNL_MAX, DCMOTOR_INIT_ERR_STR, return NULL);
+	DCMOTOR_CHECK(config->hw_info.a_timer_pins_pack < TIMER_PINS_PACK_MAX, DCMOTOR_INIT_ERR_STR, return NULL);
+	DCMOTOR_CHECK(config->hw_info.b_timer_num < TIMER_NUM_MAX, DCMOTOR_INIT_ERR_STR, return NULL);
+	DCMOTOR_CHECK(config->hw_info.b_timer_chnl < TIMER_CHNL_MAX, DCMOTOR_INIT_ERR_STR, return NULL);
+	DCMOTOR_CHECK(config->hw_info.b_timer_pins_pack < TIMER_PINS_PACK_MAX, DCMOTOR_INIT_ERR_STR, return NULL);
+
+	/* Allocate memory for handle structure */
+	dcmotor_handle_t handle = calloc(1, sizeof(dcmotor_t));
+	DCMOTOR_CHECK(handle, DCMOTOR_INIT_ERR_STR, return NULL);
+
+	/* Configure pin a */
+    pwm_cfg_t a_cfg;
+    a_cfg.timer_num = config->hw_info.a_timer_num;
+    a_cfg.timer_pins_pack = config->hw_info.a_timer_pins_pack;
+    a_cfg.timer_chnl = config->hw_info.a_timer_chnl;
+    DCMOTOR_CHECK(!pwm_config(&a_cfg), DCMOTOR_INIT_ERR_STR, {_dcmotor_cleanup(handle); return NULL;});
+
+    /* Configure pin b */
+    pwm_cfg_t b_cfg;
+    b_cfg.timer_num = config->hw_info.b_timer_num;
+    b_cfg.timer_pins_pack = config->hw_info.b_timer_pins_pack;
+    b_cfg.timer_chnl = config->hw_info.b_timer_chnl;
+    DCMOTOR_CHECK(!pwm_config(&b_cfg), DCMOTOR_INIT_ERR_STR, {_dcmotor_cleanup(handle); return NULL;});
+
+    /* Set direction */
+    if (config->dir) {
+    	DCMOTOR_CHECK(!pwm_set_params(config->hw_info.a_timer_num, config->hw_info.a_timer_chnl, config->freq, config->duty), DCMOTOR_INIT_ERR_STR, {_dcmotor_cleanup(handle); return NULL;});
+    	DCMOTOR_CHECK(!pwm_set_params(config->hw_info.b_timer_num, config->hw_info.b_timer_chnl, 0, 0), DCMOTOR_INIT_ERR_STR, {_dcmotor_cleanup(handle); return NULL;});
+    } else {
+    	DCMOTOR_CHECK(!pwm_set_params(config->hw_info.a_timer_num, config->hw_info.a_timer_chnl, 0, 0), DCMOTOR_INIT_ERR_STR, {_dcmotor_cleanup(handle); return NULL;});
+    	DCMOTOR_CHECK(!pwm_set_params(config->hw_info.b_timer_num, config->hw_info.b_timer_chnl, config->freq, config->duty), DCMOTOR_INIT_ERR_STR, {_dcmotor_cleanup(handle); return NULL;});
+    }
+
+    /* Update handle structure */
+    handle->hw_info.a_timer_num = config->hw_info.a_timer_num;
+    handle->hw_info.a_timer_chnl = config->hw_info.a_timer_chnl;
+    handle->hw_info.a_timer_pins_pack = config->hw_info.a_timer_pins_pack;
+    handle->hw_info.b_timer_num = config->hw_info.b_timer_num;
+    handle->hw_info.b_timer_chnl = config->hw_info.b_timer_chnl;
+    handle->hw_info.b_timer_pins_pack = config->hw_info.b_timer_pins_pack;
+    handle->dir = config->dir;
+    handle->freq = config->freq;
+    handle->duty = config->duty;
+    handle->lock = mutex_create();
+
+	return handle;
+}
---
+++ include/dcmotor.h
@@ -0,0 +1,66 @@
+// MIT License
+
+// Copyright (c) 2020 phonght32
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+#ifndef _DCMOTOR_H_
+#define _DCMOTOR_H_
+
+#ifdef __cplusplus 
+extern "C" {
+#endif
+
+#include "stdbool.h"
+
+#include "stm_err.h"
+#include "driver/timer.h"
+
+typedef struct dcmotor *dcmotor_handle_t;
+
+typedef struct {
+	timer_num_t					a_timer_num;			/*!< Pin A timer num */
+	timer_chnl_t				a_timer_chnl;			/*!< Pin A timer channel */
+	timer_pins_pack_t 			a_timer_pins_pack;		/*!< Pin A timer pins pack */
+	timer_num_t					b_timer_num;			/*!< Pin B timer num */
+	timer_chnl_t				b_timer_chnl;			/*!< Pin B timer channel */
+	timer_pins_pack_t 			b_timer_pins_pack;		/*!< Pin B timer pins pack */
+} dcmotor_hardware_info_t;
+
+typedef struct {
+	dcmotor_hardware_info_t 	hw_info;				/*!< Hardware information */
+	bool 						dir;					/*!< Direction */
+	uint32_t 					freq;					/*!< PWM frequency */
+	uint8_t 					duty;					/*!< PWM duty cycle */
+} dcmotor_cfg_t;
+
+/*
+ * @brief   Initialize DC motor.
+ * @param   config Struct pointer.
+ * @return
+ *      - DC motor handle structure: Success.
+ *      - 0: Fail
+ */
+dcmotor_handle_t dcmotor_init(dcmotor_cfg_t *config);
+
+#ifdef __cplusplus 
+}
+#endif
+
+#endif /* _DCMOTOR_H_ */
